321bea77669064099f2c1fc70ce3667f607e8fce
lakwon <skrdnjs1209@naver.com>
2018-06-07 13:04:45 +0900

    Update markdownItTasklist.js

--- stackedit/src/extensions/libs/markdownItTasklist.js
+++ stackedit/src/extensions/libs/markdownItTasklist.js
@@ -2,11 +2,7 @@ function attrSet(token, name, value) {
   const index = token.attrIndex(name);
   const attr = [name, value];
 
-  if (index < 0) {
-    token.attrPush(attr);
-  } else {
-    token.attrs[index] = attr;
-  }
+  index < 0 ? token.attrPush(attr) : token.attrs[index] = attr;
 }
 
 module.exports = (md) => {
